1e6c4bdac9d6c098c764839a2883f62761b8c1d6
Vitor Sbeghi <126426164+SbgVit0r@users.noreply.github.com>
2024-02-08 22:16:53 -0300

    Projeto

---
+++ test.js
@@ -0,0 +1,5 @@
+var titulo = document.querySelector("#titulo");
+
+titulo.textContent = "Lista de Encomendas";
+
+var total = document.querySelector(".info-qtd") * document.querySelector(".info-valor");
