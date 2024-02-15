dceddbaf98b609a898961e9081b42a1487be8563
Vitor Sbeghi <126426164+SbgVit0r@users.noreply.github.com>
2024-02-15 19:55:18 -0300

    Mudanças

--- test.js
+++ test.js
@@ -2,4 +2,10 @@ var titulo = document.querySelector("#titulo");
 
 titulo.textContent = "Lista de Encomendas";
 
-var total = document.querySelector(".info-qtd") * document.querySelector(".info-valor");
+const total = document.querySelectorAll('.info-total')
+const qtd = document.querySelectorAll('.info-qtd') 
+const valor = document.querySelectorAll('.info-valor')
+
+for (let i = 0; i < total.length; i++) {
+    total[i].textContent = ((qtd[i].textContent * valor[i].textContent))
+}
