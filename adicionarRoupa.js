ef7e5cf32c9c56287091a79f469267e478902a7e
Vitor Sbeghi <126426164+SbgVit0r@users.noreply.github.com>
2024-03-21 21:15:58 -0300

    Resolvendo problema

--- adicionarRoupa.js
+++ adicionarRoupa.js
@@ -2,23 +2,24 @@ var botaoAdicionar = document.querySelector("#adicionar-encomenda");
 
 
 botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+
     var linha = document.createElement("tr");
     var coluna = document.createElement("td");
     var coluna2 = document.createElement("td");
     var coluna3 = document.createElement("td");
     var coluna4 = document.createElement("td");
     var coluna5 = document.createElement("td");
-    var nome =  document.getElementsByClassName(".info-nome").value;
-    var qtde = document.getElementsByClassName(".info-produto").value;
-    var produto = document.getElementsByClassName(".info-qtd").value;
-    var preco = document.getElementsByClassName(".info-valor").value;
+    var nome =  document.querySelector("#nome").value;
+    var produto  = document.querySelector("#produto").value;
+    var qtde = document.querySelector("#qtd").value;
+    var preco = parseFloat(document.querySelector("#valor").value);
     var total = calculaTotal(preco, qtde)
-    event.preventDefault();
 
     coluna.textContent = nome;
-    coluna2.textContent = qtde;
-    coluna3.textContent = produto;
-    coluna4.textContent = preco;
+    coluna2.textContent = produto;
+    coluna3.textContent = qtde;
+    coluna4.textContent = formataValor(preco);
     coluna5.textContent = total;
 
     linha.appendChild(coluna);
